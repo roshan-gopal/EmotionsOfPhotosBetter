0908a9246f9f66ce79488025ee45a78ac660305a
roshan-gopal <roshangopal06@gmail.com>
2025-03-04 02:40:19 -0500

    Added Image for URL

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,29 @@
-"use client"
+
+
+export const metadata = {
+  title: 'Photo Emotions Analyzer',
+  description: 'Discover the emotions your photos convey using AI analysis',
+  openGraph: {
+    title: 'Niepce',
+    description: 'Discover the emotions your photos give',
+    images: [
+      {
+        url: '/niepce.png',
+        width: 1200,
+        height: 630,
+        alt: 'Photo Emotions Analyzer Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Niepce',
+    description: 'Discover the emotions your photos give',
+    images: ['/niepce.png'],
+  },
+};
 
 export default function RootLayout({ children }) {
     return (
