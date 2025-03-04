9f6e74f70107c686447a09b24809364cc8300429
roshan-gopal <roshangopal06@gmail.com>
2025-03-04 13:58:37 -0500

    Fixed Preview1

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,20 +6,18 @@ export const metadata = {
     description: 'Discover the emotions your photos give',
     images: [
       {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL || ''}/Niepce.png`,
+        url: `https://emotions-of-photos-better-final.vercel.app/Niepce.png`,
         width: 1200,
         height: 630,
         alt: 'Photo Emotions Analyzer Preview',
       },
     ],
-    locale: 'en_US',
-    type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Niepce',
     description: 'Discover the emotions your photos give',
-    images: [`${process.env.NEXT_PUBLIC_BASE_URL || ''}/Niepce.png`],
+    images: [`https://emotions-of-photos-better-final.vercel.app/Niepce.png`],
   },
 };
 
