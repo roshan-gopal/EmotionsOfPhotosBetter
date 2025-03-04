c574f048ba07294926a0ffeeb1b9704560e1b6c6
roshan-gopal <roshangopal06@gmail.com>
2025-03-04 16:00:11 -0500

    Fixed Image Preview

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,9 +4,11 @@ export const metadata = {
   openGraph: {
     title: 'Niepce',
     description: 'Discover the emotions your photos give',
+    type: 'website',
+    url: 'https://emotions-of-photos-better-final.vercel.app',
     images: [
       {
-        url: `https://emotions-of-photos-better-final.vercel.app/Niepce.png`,
+        url: 'https://emotions-of-photos-better-final.vercel.app/Niepce.png',
         width: 1200,
         height: 630,
         alt: 'Photo Emotions Analyzer Preview',
@@ -17,7 +19,7 @@ export const metadata = {
     card: 'summary_large_image',
     title: 'Niepce',
     description: 'Discover the emotions your photos give',
-    images: [`https://emotions-of-photos-better-final.vercel.app/Niepce.png`],
+    images: ['https://emotions-of-photos-better-final.vercel.app/Niepce.png'],
   },
 };
 
